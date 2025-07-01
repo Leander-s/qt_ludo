f9efd147a50008b615f86d2e1aba1fd479d2ea4a
Leander <leander.funken@live.de>
2025-07-01 22:33:48 +0200

    updated shaders

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -1,8 +1,12 @@
 #version 330 core
 
 in vec3 vColor;
+in vec2 vUV;
 out vec4 fragColor;
 
+uniform sampler2D tex;
+
 void main(){
-    fragColor = vec4(vColor, 1.0f);
+    vec4 texColor = texture(tex, vUV);
+    fragColor = texColor + vec4(vColor, 1.0f);
 }
